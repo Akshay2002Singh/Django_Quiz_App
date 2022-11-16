61e00ee1fa95f96074f6352cddc8e3202435fa6b
Akshay2002Singh <06akshay2002@gmail.com>
2022-11-17 02:10:06 +0530

    Starter template

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+path("",views.index,name="home"),
+path("log_out",views.log_out,name="log_out"),
+path("login",views.login_user,name="login_user"),
+path("login_user_form",views.login_form,name="login_form"),
+path("signup",views.create_user,name="create"),
+path("create_user_form",views.create_user_form,name="create_form"),
+path("contact_us",views.contact_us,name="contact_us"),
+path("submit_contact_form",views.submit_contact_form,name="submit_contact_form"),
+]
